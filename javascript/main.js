aac1d63327d2a1be64849e60838e40e5ee9b1037
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-01-15 10:49:45 -0500

    Cors Issue?

--- javascript/main.js
+++ javascript/main.js
@@ -10,7 +10,11 @@ tiles.addTo(mymap);
 async function fetchLocation(ip) {
         var value;
         await fetch('https://geo.ipify.org/api/v1?apiKey=at_KvK9f5u7StYcnwQVhloWnCdQkOWDK&ipAddress=' + ip, {
-          method: 'GET'
+          method: 'GET',
+          mode: 'cors',
+          headers: {
+            'Access-Control-Allow-Origin':'*'
+          }
         })
           .then(response => { value = response.json()})
           .catch(err => {
